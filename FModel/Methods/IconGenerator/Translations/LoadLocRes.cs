273fab7e3918e4f398d9951dc79af02d1319e27b
Asval <valentinbaudry64@gmail.com>
2019-06-18 23:48:50 +0200

    searching weapon stats improved by 1 second + locres fix again

--- FModel/Methods/IconGenerator/Translations/LoadLocRes.cs
+++ FModel/Methods/IconGenerator/Translations/LoadLocRes.cs
@@ -30,14 +30,24 @@ namespace FModel
             }
         }
 
+        /// <summary>
+        /// 1. if loading a dynamic pak we have to switch between keys because the translation file is the main paks hence string oldKey is there
+        /// 2. smh if loading a dynamic pak, the guid isn't reset when registering, the temp solution is to fake the guid
+        /// </summary>
+        /// <param name="selectedLanguage"></param>
+        /// <returns></returns>
         private static string getMyLocRes(string selectedLanguage)
         {
-            if (ThePak.AllpaksDictionary != null)
+            if (ThePak.AllpaksDictionary != null && ThePak.AllpaksDictionary["Game_BR.locres"] != null)
             {
-                string oldKey = JohnWick.MyKey;
-                JohnWick.MyKey = Properties.Settings.Default.AESKey;
+                string oldKey = JohnWick.MyKey; //get the old key
+
+                JohnWick.MyKey = Properties.Settings.Default.AESKey; //set the main key to extract
+                ThePak.CurrentUsedPakGuid = "0-0-0-0"; //fake the guid -> writeFile need this guid to get the mountPoint, otherwise it crashes
+
                 string locResPath = JohnWick.ExtractAsset(ThePak.AllpaksDictionary["Game_BR.locres"], "Game_BR.locres");
-                JohnWick.MyKey = oldKey;
+
+                JohnWick.MyKey = oldKey; //set the old key
 
                 return LocResSerializer.StringFinder(locResPath.Replace("zh-Hant", selectedLanguage));
             }
