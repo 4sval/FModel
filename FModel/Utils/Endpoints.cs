f5fa499905a0c9d0ca5a5f915a96e413e2a17f39
Not Officer <marlon.john007@gmail.com>
2020-10-10 20:32:47 +0200

    added playlist images support

--- FModel/Utils/Endpoints.cs
+++ FModel/Utils/Endpoints.cs
@@ -3,20 +3,41 @@ using Newtonsoft.Json;
 using System;
 using System.IO;
 using System.Net.Http;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+
+using Fortnite_API;
 
 namespace FModel.Utils
 {
     static class Endpoints
     {
+        public static readonly FortniteApi FortniteAPI = new FortniteApi($"FModel/{Assembly.GetExecutingAssembly().GetName().Version}");
         public const string BENBOT_AES = "https://benbotfn.tk/api/v1/aes";
         public const string BENBOT_HOTFIXES = "https://benbotfn.tk/api/v1/hotfixes";
         public const string FMODEL_JSON = "https://dl.dropbox.com/s/sxyaqo6zu1drlea/FModel.json?dl=0";
         public const string OAUTH_URL = "https://account-public-service-prod03.ol.epicgames.com/account/api/oauth/token";
         private const string _BASIC_FN_TOKEN = "basic MzQ0NmNkNzI2OTRjNGE0NDg1ZDgxYjc3YWRiYjIxNDE6OTIwOWQ0YTVlMjVhNDU3ZmI5YjA3NDg5ZDMxM2I0MWE=";
 
-        public static async Task<string> GetStringEndpoint(string url) => await GetStringEndpoint(url, string.Empty);
+        public static byte[] GetRawData(Uri uri) => GetRawDataAsync(uri).GetAwaiter().GetResult();
+        public static async Task<byte[]> GetRawDataAsync(Uri uri)
+        {
+            using (HttpClient client = new HttpClient { Timeout = TimeSpan.FromSeconds(2) })
+            {
+                try
+                {
+                    var data = await client.GetByteArrayAsync(uri).ConfigureAwait(false);
+                    return data;
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+
+        public static Task<string> GetStringEndpoint(string url) => GetStringEndpoint(url, string.Empty);
         public static async Task<string> GetStringEndpoint(string url, string token)
         {
             DebugHelper.WriteLine("{0} {1} {2} {3}", "[FModel]", "[Endpoints]", "[GET]", url);
@@ -49,7 +70,7 @@ namespace FModel.Utils
             return string.Empty;
         }
 
-        public static async Task<T> GetJsonEndpoint<T>(string url) => await GetJsonEndpoint<T>(url, string.Empty);
+        public static Task<T> GetJsonEndpoint<T>(string url) => GetJsonEndpoint<T>(url, string.Empty);
         public static async Task<T> GetJsonEndpoint<T>(string url, string query)
         {
             if (url.Equals(BENBOT_HOTFIXES) && !string.IsNullOrEmpty(query))
