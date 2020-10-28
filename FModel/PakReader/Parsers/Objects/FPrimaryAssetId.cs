220ed024cd6ef8bb876151d39a6b0099b2fcd444
Fabian <mcdtloeser@gmail.com>
2020-10-28 21:57:37 +0100

    added some missing structs

---
+++ FModel/PakReader/Parsers/Objects/FPrimaryAssetId.cs
@@ -0,0 +1,14 @@
+﻿namespace FModel.PakReader.Parsers.Objects
+{
+    public readonly struct FPrimaryAssetId : IUStruct
+    {
+        public readonly FPrimaryAssetType PrimaryAssetType;
+        public readonly FName PrimaryAssetName;
+
+        public FPrimaryAssetId(PackageReader reader)
+        {
+            PrimaryAssetType = new FPrimaryAssetType(reader);
+            PrimaryAssetName = reader.ReadFName();
+        }
+    }
+}
---
+++ FModel/PakReader/Parsers/Objects/FPrimaryAssetType.cs
@@ -0,0 +1,12 @@
+﻿namespace FModel.PakReader.Parsers.Objects
+{
+    public readonly struct FPrimaryAssetType : IUStruct
+    {
+        public readonly FName Name;
+
+        public FPrimaryAssetType(PackageReader reader)
+        {
+            Name = reader.ReadFName();
+        }
+    }
+}
