fee89ebca843a5fe9fe9bdf60b29b747b7f3f255
iAmAsval <valentinbaudry64@gmail.com>
2021-12-26 23:05:50 +0100

    FModel v4.2.1

--- FModel/Views/ImageMerger.xaml.cs
+++ FModel/Views/ImageMerger.xaml.cs
@@ -144,7 +144,7 @@ namespace FModel.Views
             var fileBrowser = new OpenFileDialog
             {
                 Title = "Add image(s)",
-                InitialDirectory = $"{UserSettings.Default.OutputDirectory}\\Textures",
+                InitialDirectory = $"{UserSettings.Default.OutputDirectory}\\Exports",
                 Multiselect = true,
                 Filter = "Image Files (*.png,*.bmp,*.jpg,*.jpeg,*.jfif,*.jpe,*.tiff,*.tif)|*.png;*.bmp;*.jpg;*.jpeg;*.jfif;*.jpe;*.tiff;*.tif|All Files (*.*)|*.*"
             };
