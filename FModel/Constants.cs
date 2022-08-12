f8e929b3477b3ddc110d8532c1606c785fe001b9
4sval <asval.contactme@gmail.com>
2022-08-13 00:39:52 +0200

    reworked how exceptions were displayed

--- FModel/Constants.cs
+++ FModel/Constants.cs
@@ -10,14 +10,15 @@ public static class Constants
     public static readonly FGuid ZERO_GUID = new(0U);
 
     public const string WHITE = "#DAE5F2";
+    public const string GRAY = "#BBBBBB";
     public const string RED = "#E06C75";
     public const string GREEN = "#98C379";
     public const string YELLOW = "#E5C07B";
     public const string BLUE = "#528BCC";
 
     public const string ISSUE_LINK = "https://github.com/iAmAsval/FModel/issues/new/choose";
-    public const string DONATE_LINK = "https://fmodel.app/donate";
-    public const string DISCORD_LINK = "https://fmodel.app/discord";
+    public const string DONATE_LINK = "https://fmodel.app/donate?utm_source=fmodel&utm_medium=app&utm_campaign=donations";
+    public const string DISCORD_LINK = "https://fmodel.app/discord?utm_source=fmodel&utm_medium=app&utm_campaign=discord-server";
 
     public const string _FN_LIVE_TRIGGER = "fortnite-live.manifest";
     public const string _VAL_LIVE_TRIGGER = "valorant-live.manifest";
--- FModel/ViewModels/ThreadWorkerViewModel.cs
+++ FModel/ViewModels/ThreadWorkerViewModel.cs
@@ -1,7 +1,7 @@
 using System;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using FModel.Extensions;
 using FModel.Framework;
 using FModel.Services;
 using FModel.Views.Resources.Controls;
@@ -41,6 +41,10 @@ public class ThreadWorkerViewModel : ViewModel
 
     private ApplicationViewModel _applicationView => ApplicationService.ApplicationView;
     private readonly AsyncQueue<Action<CancellationToken>> _jobs;
+    private const string _at = "   at ";
+    private const char _dot = '.';
+    private const char _colon = ':';
+    private const string _gray = "#999";
 
     public ThreadWorkerViewModel()
     {
@@ -99,8 +103,32 @@ public class ThreadWorkerViewModel : ViewModel
                     Log.Error("{Exception}", e);
 
                     FLogger.AppendError();
-                    FLogger.AppendText(e.Message, Constants.WHITE, true);
-                    FLogger.AppendText("      " + e.StackTrace.SubstringBefore('\n').Trim(), Constants.WHITE, true);
+                    if ((e.InnerException ?? e) is { TargetSite.DeclaringType: not null } exception)
+                    {
+                        if (exception.TargetSite.ToString() == "CUE4Parse.FileProvider.GameFile get_Item(System.String)")
+                        {
+                            FLogger.AppendText(e.Message, Constants.WHITE, true);
+                        }
+                        else
+                        {
+                            var t = exception.GetType();
+                            FLogger.AppendText(t.Namespace + _dot, Constants.GRAY);
+                            FLogger.AppendText(t.Name, Constants.WHITE);
+                            FLogger.AppendText(_colon + " ", Constants.GRAY);
+                            FLogger.AppendText(exception.Message, Constants.RED, true);
+
+                            FLogger.AppendText(_at, _gray);
+                            FLogger.AppendText(exception.TargetSite.DeclaringType.FullName + _dot, Constants.GRAY);
+                            FLogger.AppendText(exception.TargetSite.Name, Constants.YELLOW);
+
+                            var parameters = new StringBuilder();
+                            foreach (var parameter in exception.TargetSite.GetParameters())
+                            {
+                                parameters.Append(parameter.ParameterType.Name + " " + parameter.Name);
+                            }
+                            FLogger.AppendText("(" + string.Join(", ", parameters) + ")", Constants.GRAY, true);
+                        }
+                    }
                     return;
                 }
             }
