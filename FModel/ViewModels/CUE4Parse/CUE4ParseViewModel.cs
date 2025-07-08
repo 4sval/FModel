43fb6bd8a01e6b340851677e193343502f9ca376
Masusder <59669685+Masusder@users.noreply.github.com>
2025-07-08 19:34:57 +0200

    Merge remote-tracking branch 'upstream/dev' into feature/build-diff-viewer

--- FModel/ViewModels/CUE4Parse/CUE4ParseViewModel.cs
+++ FModel/ViewModels/CUE4Parse/CUE4ParseViewModel.cs
@@ -14,6 +14,7 @@ using CUE4Parse.FileProvider.Vfs;
 using CUE4Parse.GameTypes.KRD.Assets.Exports;
 using CUE4Parse.UE4.AssetRegistry;
 using CUE4Parse.UE4.Assets;
+using CUE4Parse.UE4.BinaryConfig;
 using CUE4Parse.UE4.Assets.Exports;
 using CUE4Parse.UE4.Assets.Exports.Animation;
 using CUE4Parse.UE4.Assets.Exports.Material;
@@ -289,6 +290,16 @@ public partial class CUE4ParseViewModel : ViewModel
                         if (CheckExport(cancellationToken, result.Package, i, bulk))
                             break;
                     }
+
+                    break;
+                }
+            case "ini" when entry.Name.Contains("BinaryConfig"):
+                {
+                    var ar = entry.CreateReader();
+                    var configCache = new FConfigCacheIni(ar);
+
+                    TabControl.SelectedTab.Highlighter = AvalonExtensions.HighlighterSelector("json");
+                    TabControl.SelectedTab.SetDocumentText(JsonConvert.SerializeObject(configCache, Formatting.Indented), saveProperties, updateUi);
 
                     break;
                 }
