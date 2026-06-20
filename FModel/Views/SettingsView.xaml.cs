89f9593c5b23638db1131ec2b94fb8b8c1827a72
yarik0chka <yarik0chka@gmail.com>
2026-06-20 12:54:54 +0500

    Auto-refresh endpoint on save

--- FModel/Views/SettingsView.xaml.cs
+++ FModel/Views/SettingsView.xaml.cs
@@ -198,24 +198,36 @@ public partial class SettingsView
         _applicationView.SettingsView.SelectedMapStructTypes = editor.MapStructTypes;
     }
 
-    private void OpenAesEndpoint(object sender, RoutedEventArgs e)
+    private async void OpenAesEndpoint(object sender, RoutedEventArgs e)
     {
         var editor = new EndpointEditor(
             _applicationView.SettingsView.AesEndpoint, "Endpoint Configuration (AES)", EEndpointType.Aes);
         if (_applicationView.Status.IsReady)
             _applicationView.Status.SetStatus(EStatusKind.Configuring);
         editor.ShowDialog();
+        if (editor.DialogResult == true)
+        {
+            await _applicationView.CUE4Parse.RefreshAes();
+            await _applicationView.AesManager.InitAes();
+            _applicationView.AesManager.HasChange = true;
+            await _applicationView.UpdateProvider(false);
+        }
         if (_applicationView.Status.IsReady)
             _applicationView.Status.SetStatus(EStatusKind.Ready);
     }
 
-    private void OpenMappingEndpoint(object sender, RoutedEventArgs e)
+    private async void OpenMappingEndpoint(object sender, RoutedEventArgs e)
     {
         var editor = new EndpointEditor(
             _applicationView.SettingsView.MappingEndpoint, "Endpoint Configuration (Mapping)", EEndpointType.Mapping);
         if (_applicationView.Status.IsReady)
             _applicationView.Status.SetStatus(EStatusKind.Configuring);
         editor.ShowDialog();
+        if (editor.DialogResult == true)
+        {
+            await _applicationView.CUE4Parse.InitMappings(true);
+            await _applicationView.UpdateProvider(false);
+        }
         if (_applicationView.Status.IsReady)
             _applicationView.Status.SetStatus(EStatusKind.Ready);
     }
